a8e76ef83145bd2cd55fa1c25618c88ef7ebde23
helena_19 <sdi1300091@di.uoa.gr>
2017-04-01 05:02:36 +0300

    4 done

--- Main.java
+++ Main.java
@@ -89,9 +89,8 @@ public class Main {
 			totalLines += n;
 		}
 		/*----------------------------END OF 2--------------------------------*/
-	}
-}
- /*----------------------------------3---------------------------------*/
+
+	 	/*----------------------------------3---------------------------------*/
         //--------Branches
 		command = "git branch";
 		commandResults = execute(command, filePath, null);
@@ -106,5 +105,81 @@ public class Main {
         for (i = 0; i < numOfBranches; ++i) {
             if (!branchesNames[i].contains("->")) realNumber++;
         }
-/*----------------------------END OF 3--------------------------------*/
+		/*----------------------------END OF 3--------------------------------*/
 
+		/*----------------------------------4---------------------------------*/
+        for (i = 0; i < numOfBranches; ++i) {
+            if (branchesNames[i].startsWith("*")) continue;
+            if (branchesNames[i].contains("->")) continue;
+            command = "git log --reverse " + branchesNames[i];                  //commits -> first commit is first
+    		commandResults = execute(command, filePath, null);
+            String commitsR[] = commandResults.split("\\r?\\n");
+            String firstCommit = commitsR[2];
+            firstCommit = firstCommit.replace("Date:   ","");
+            command = "git log " + branchesNames[i];                            //commits -> last commit is first
+    		commandResults = execute(command, filePath, null);
+            String commitsN[] = commandResults.split("\\r?\\n");
+            String lastCommit = commitsN[2];
+            lastCommit = lastCommit.replace("Date:   ","");
+
+            HTMLFile += "<td><a href=\"" + branchesNames[i] + ".html\">";
+            HTMLFile += branchesNames[i] + "</a></td>";
+            HTMLFile += "<td>" + firstCommit + "</td>";
+            HTMLFile += "<td>"+ lastCommit + "</td>";
+
+            //--------i
+
+            String HTMLBranch = branchHTML1(args);
+            HTMLBranch += "&nbsp;" + branchesNames[i] + "</h3>";
+            for (j = 0; j < commitsR.length; ++j) {
+                if (commitsR[j].startsWith("commit")) {
+                    HTMLBranch += "<br/>";
+                    HTMLBranch += "<h4><img alt=\"My Image7\" src=\"commit.png\" width=\"25\" height=\"25\"/>&nbsp;";
+                    HTMLBranch += commitsR[j] + "</h4>";
+                    HTMLBranch += "<hr>";
+                }
+                else if (commitsR[j].startsWith("Author:")){
+                    HTMLBranch += "<h5><img alt=\"My Image8\" src=\"user.png\" width=\"25\" height=\"25\"/>&nbsp;" + commitsR[j] + "</h5>";
+                }
+                else if (commitsR[j].startsWith("Date:")) {
+                    HTMLBranch += "<h5><img alt=\"My Image9\" src=\"callendar.jpeg\" width=\"25\" height=\"25\"/>&nbsp;" + commitsR[j] + "</h5>";
+                }
+                else {
+                    HTMLBranch += "<h6>" + commitsR[j] + "</h6>";
+                }
+            }
+            HTMLBranch += "</form></body></html>";
+            for (j = 0; j < commitsR.length; ++j) {
+            }
+            //--------End of i
+            //--------ii
+            command = "git tag --contains " + branchesNames[i];
+            commandResults = execute(command, filePath, null);
+
+            HTMLBranch += "Tags " + commandResults + "<br/>";
+            HTMLBranch += "</li>";
+            HTMLBranch += "</div></div></div></div></form></body></html>";
+
+            BufferedWriter writer = null;
+            String where = args[1] + "/" + branchesNames[i] + ".html";
+            String w2 = where.replace(" ", "");
+            try {
+                writer = new BufferedWriter(new FileWriter(w2));
+                writer.write(HTMLBranch);
+            }
+            catch ( IOException e) {
+            }
+            finally {
+                try {
+                    if (writer != null)
+                    writer.close();
+                }
+                catch (IOException e)
+                {
+                }
+            }
+            //--------End of ii
+        }
+        /*----------------------------END OF 4--------------------------------*/
+	}
+}
